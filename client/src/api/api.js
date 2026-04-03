73a9a0584a61554907d384e9b4e69649f730d0f5
YASH <yashsanjay.k21@iiits.in>
2026-04-03 09:49:58 +0530

    api file changed with backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://one-play-sports-assignment.onrender.com"
 });
 
 export default API;
